c93c2b0895f789b8897e183c7dfdf950b6a5a16c
Saup21 <sauparna.gupta@gmail.com>
2022-02-01 02:27:29 +0530

    better regex added

--- scripts/isValidUrl.ts
+++ scripts/isValidUrl.ts
@@ -1,13 +1,15 @@
 // function to validate if a string contains a url or not
 
 const isValidUrl = (s: string): string[] => {
-    const matchpattern = new RegExp(`([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?`);
+    const matchpattern = new RegExp(`(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})`); // Better regex found
+
+    // ([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)? // Previous regex
 
     let matches: string[] = [];
     let split_s: string[] = s.split(" ");
 
     split_s.forEach( (check: string) => {
-        if(matchpattern.test(check)) {
+        if(matchpattern.test(check) && check[0] === 'h') {
             matches.push(check);
         }
     });
